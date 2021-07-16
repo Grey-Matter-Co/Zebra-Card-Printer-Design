ba15fd29f33a3efa6ffb19bdf700800e38e4b6e3
M1ndBlast <gustavopdzz0@gmail.com>
2021-07-16 18:59:19 -0500

    Realizando preview en template

--- app/src/main/java/mx/com/infotecno/zebracardprinter/ui/printtemplate/PrintTemplateFragment.kt
+++ app/src/main/java/mx/com/infotecno/zebracardprinter/ui/printtemplate/PrintTemplateFragment.kt
@@ -1,6 +1,5 @@
 package mx.com.infotecno.zebracardprinter.ui.printtemplate
 
-import android.R.attr.bitmap
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -15,6 +14,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -28,7 +28,6 @@ import mx.com.infotecno.zebracardprinter.databinding.PrinttemplateFragmentBindin
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import mx.com.infotecno.zebracardprinter.util.ExecutingDevicesHelper as EDHelper
-
 
 class PrintTemplateFragment : Fragment() {
 
@@ -55,8 +54,6 @@ class PrintTemplateFragment : Fragment() {
 
 	override fun onActivityCreated(savedInstanceState: Bundle?) {
 		super.onActivityCreated(savedInstanceState)
-
-
 //		setupUiComponents()
 	}
 
@@ -117,7 +114,9 @@ class PrintTemplateFragment : Fragment() {
 					binding.fieldsContainerLlyt.addView(addTemplateFieldView(key))
 				}
 
-				Log.d("EMBY", "handleAction: ${action.template}")
+				viewModel.testTemp(args.template.name, action.zCardTemplate.fields)
+
+				Log.d("EMBY", "handleAction: ${action.template.Sides[0]}")
 			}
 
 			is ZCardTemplatePrintAction.CameraCapture -> // Opening Camera
@@ -148,6 +147,9 @@ class PrintTemplateFragment : Fragment() {
 					override fun afterTextChanged(editable: Editable)
 						{ fields[fieldName] = editable.toString() }
 				})
+//				findViewById<TextInputEditText>(R.id.inputText).setOnFocusChangeListener { _, hasFocus ->
+//					Toast.makeText(activity,  if (hasFocus) "focused" else "focuse lose", Toast.LENGTH_LONG).show()
+//				}
 			}
 		}
 	}
--- app/src/main/java/mx/com/infotecno/zebracardprinter/ui/printtemplate/PrintTemplateViewModel.kt
+++ app/src/main/java/mx/com/infotecno/zebracardprinter/ui/printtemplate/PrintTemplateViewModel.kt
@@ -1,21 +1,27 @@
 package mx.com.infotecno.zebracardprinter.ui.printtemplate
 
 import android.app.Application
+import android.content.ClipData.Item
 import android.content.ContentResolver
 import android.database.ContentObserver
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Handler
 import android.provider.MediaStore
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.zebra.sdk.common.card.template.ZebraCardTemplate
 import kotlinx.coroutines.launch
+import mx.com.infotecno.zebracardprinter.MainActivity
 import mx.com.infotecno.zebracardprinter.action.ZCardTemplatePrintAction
-import mx.com.infotecno.zebracardprinter.model.XMLCardTemplate
 import mx.com.infotecno.zebracardprinter.model.ZCardTemplate
 import mx.com.infotecno.zebracardprinter.util.FileHelper
+import java.io.File
 import mx.com.infotecno.zebracardprinter.util.ExecutingDevicesHelper as EDHelper
+
 
 class PrintTemplateViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -27,10 +33,11 @@ class PrintTemplateViewModel(application: Application) : AndroidViewModel(applic
 
 //	private val _actions = mutableMapOf<String, Any>()    // This will be operated and changed
 //	val action: Map<String, Any> get() = this._actions      // This only returns unmodified list
-//	val zebraCardTemplate: ZebraCardTemplate = ZebraCardTemplate(getApplication(), null).apply {
-//		this.setTemplateFileDirectory(getApplication<Application>().filesDir.path+ File.separator + MainActivity.TEMPLATEFILEDIRECTORY)
-//		this.setTemplateImageFileDirectory(getApplication<Application>().filesDir.path+ File.separator + MainActivity.TEMPLATEIMAGEFILEDIRECTORY)
-//	}
+	var zebraCardTemplate: ZebraCardTemplate = ZebraCardTemplate(getApplication(), null).apply {
+		Log.d("EMBY", "zebraCardTemplate: ${getApplication<Application>().filesDir.path+ File.separator + MainActivity.TEMPLATEFILEDIRECTORY}")
+		this.setTemplateFileDirectory(getApplication<Application>().filesDir.path+ File.separator + MainActivity.TEMPLATEFILEDIRECTORY)
+		this.setTemplateImageFileDirectory(getApplication<Application>().filesDir.path+ File.separator + MainActivity.TEMPLATEIMAGEFILEDIRECTORY)
+	}
 
 	fun loadTemplate(zCardTemplate: ZCardTemplate? = null) {
 		viewModelScope.launch {
@@ -64,5 +71,43 @@ class PrintTemplateViewModel(application: Application) : AndroidViewModel(applic
 		}
 		registerContentObserver(uri, true, contentObserver)
 		return contentObserver
+	}
+
+	fun testTemp(name: String, fields: Map<String, Any>) {
+		val templateFieldKeys = zebraCardTemplate.getTemplateFields(name)
+
+
+		var list: List<Item>
+		val map: MutableMap<String, String> = HashMap()
+		for (i in templateFieldKeys) map[i] = ""
+
+		zebraCardTemplate.getTemplateFields(name).forEach {
+			Log.d("EMBY", "testTemp var: $it")
+		}
+
+		val graphicsData = zebraCardTemplate.generateTemplateJob(name, map).graphicsData
+		for (info in graphicsData) {
+			if (info.graphicData != null) {
+				val imageData = info.graphicData.imageData
+				val bitmap = BitmapFactory.decodeByteArray(imageData, 0, imageData.size)
+				templatePreviewList.add(
+					TemplatePreview(
+						weakContext.get().getString(R.string.card_side_and_type, info.side, info.printType),
+						bitmap
+					)
+				)
+				bitmap.recycle()
+			} else {
+				templatePreviewList.add(
+					TemplatePreview(
+						weakContext.get().getString(R.string.card_side_and_type, info.side, info.printType),
+						weakContext.get().getString(R.string.no_image_data_found)
+					)
+				)
+			}
+		}
+
+
+
 	}
 }
